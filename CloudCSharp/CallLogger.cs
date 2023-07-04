fd5c7d30b3a6aeeed39306d2bcc70fd85a4dbf79
Rellik_Pan <pan820124@gmail.com>
2023-07-04 17:25:38 +0800

    feat: 🎸 aop template

---
+++ CloudCSharp/CallLogger.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Castle.DynamicProxy;
+
+namespace cloudcsharp.CloudCSharp
+{
+    public class CallLogger : IInterceptor
+
+    {
+        TextWriter _output;
+
+        public CallLogger(TextWriter output)
+        {
+            _output = output;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+
+            _output.WriteLine("你正在调用方法 \"{0}\" ",
+              invocation.Method.Name);
+            try
+            {
+                //在被拦截的方法执行完毕后 继续执行
+                invocation.Proceed();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            _output.WriteLine("Interceptor End.");
+        }
+    }
+
+    public interface IShape
+    {
+        void Area();
+    }
+
+    public class Circle : IShape
+    {
+        public void Area()
+        {
+            Console.WriteLine("Circle");
+            throw new Exception("Circle GGGG"); // 強迫噴錯
+        }
+    }
+
+    public class Square : IShape
+    {
+        public void Area()
+        {
+            Console.WriteLine("Square");
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,15 +1,31 @@
-﻿
-class Program
+﻿// using Autofac;
+// using Autofac.Extras.DynamicProxy;
+using cloudcsharp.CloudCSharp;
+using Castle.DynamicProxy;
+
+public class Program
 {
-    static void Main()
+    public static void Main(string[] args)
     {
-        try
-        {
-            /// run command
-        }
-        catch
-        {
-            throw;
-        }
+        // // 建立 Autofac 容器
+        // var builder = new ContainerBuilder();
+        // builder.RegisterType<Circle>().As<IShape>().InterceptedBy(typeof(CallLogger)).EnableInterfaceInterceptors();
+
+        // builder.Register(c => new CallLogger(Console.Out));
+
+        // var container = builder.Build();
+        // var circle = container.Resolve<IShape>();
+        // circle.Area();
+
+        var proxyGenerator = new ProxyGenerator();
+        var callLogger = new CallLogger(Console.Out);
+
+        // 宣告兩個同 interface 的不同 class
+        var circle = proxyGenerator.CreateInterfaceProxyWithTarget<IShape>(new Circle(), callLogger);
+        var square = proxyGenerator.CreateInterfaceProxyWithTarget<IShape>(new Square(), callLogger);
+
+
+        circle.Area();
+        square.Area();
     }
 }
